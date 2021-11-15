f15b9368b1cc2698ae2008f1e42a7d5d28f7f831
Davith Shamiryan <dshamiryan@list.ru>
2021-11-15 18:58:01 +0400

    Add project files.

---
+++ TarakanApp/TarakanForm.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace TarakanApp
+{
+    partial class TarakanForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TarakanForm));
+            this.tarakan = new System.Windows.Forms.Button();
+            this.tarakan1 = new System.Windows.Forms.Button();
+            this.tarakan2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.startbutton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tarakan
+            // 
+            this.tarakan.Location = new System.Drawing.Point(85, 55);
+            this.tarakan.Name = "tarakan";
+            this.tarakan.Size = new System.Drawing.Size(83, 23);
+            this.tarakan.TabIndex = 0;
+            this.tarakan.Text = "Tarakan";
+            this.tarakan.UseVisualStyleBackColor = true;
+            // 
+            // tarakan1
+            // 
+            this.tarakan1.Location = new System.Drawing.Point(85, 160);
+            this.tarakan1.Name = "tarakan1";
+            this.tarakan1.Size = new System.Drawing.Size(83, 23);
+            this.tarakan1.TabIndex = 1;
+            this.tarakan1.Text = "Tarakan1";
+            this.tarakan1.UseVisualStyleBackColor = true;
+            // 
+            // tarakan2
+            // 
+            this.tarakan2.Location = new System.Drawing.Point(85, 259);
+            this.tarakan2.Name = "tarakan2";
+            this.tarakan2.Size = new System.Drawing.Size(83, 23);
+            this.tarakan2.TabIndex = 2;
+            this.tarakan2.Text = "Tarakan2";
+            this.tarakan2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(681, 55);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(107, 330);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // startbutton
+            // 
+            this.startbutton.Location = new System.Drawing.Point(349, 404);
+            this.startbutton.Name = "startbutton";
+            this.startbutton.Size = new System.Drawing.Size(75, 23);
+            this.startbutton.TabIndex = 4;
+            this.startbutton.Text = "Start";
+            this.startbutton.UseVisualStyleBackColor = true;
+            this.startbutton.Click += new System.EventHandler(this.startbutton_Click);
+            // 
+            // TarakanForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.startbutton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.tarakan2);
+            this.Controls.Add(this.tarakan1);
+            this.Controls.Add(this.tarakan);
+            this.Name = "TarakanForm";
+            this.Text = "TarakanForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button tarakan;
+        private System.Windows.Forms.Button tarakan1;
+        private System.Windows.Forms.Button tarakan2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button startbutton;
+    }
+}
+
---
+++ TarakanApp/TarakanForm.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TarakanApp
+{
+    public partial class TarakanForm : Form
+    {
+        public TarakanForm()
+        {
+            InitializeComponent();
+
+        }
+        Random random = new Random();
+        Thread threadTarakan;
+        Thread threadTarakan1;
+        Thread threadTarakan2;
+
+        Point tarakanpoint;
+        Point tarakan1point;
+        Point tarakan2point;
+
+        private void startbutton_Click(object sender, EventArgs e)
+        {
+            tarakanpoint = new Point(tarakan.Location.X, tarakan.Location.Y);
+            tarakan1point = new Point(tarakan1.Location.X, tarakan1.Location.Y);
+            tarakan2point = new Point(tarakan2.Location.X, tarakan2.Location.Y);
+            threadTarakan = new Thread(TarakanMove);
+            threadTarakan.Start();
+            threadTarakan1 = new Thread(Tarakan1Move);
+            threadTarakan1.Start();
+            threadTarakan2 = new Thread(Tarakan2Move);
+            threadTarakan2.Start();
+        }
+        private void TarakanMove()
+        {
+            while (tarakan.Location.X <= pictureBox1.Location.X - tarakan.Width + 1)
+            {
+                Thread.Sleep(random.Next(10, 50));
+
+                this.Invoke((MethodInvoker)delegate
+                {
+                    tarakan.Location = new Point(tarakan.Location.X + random.Next(1, 5), tarakan.Location.Y);
+                });
+            }
+           TarakanFinish();
+        }
+        private void Tarakan1Move()
+        {
+            while (tarakan1.Location.X <= pictureBox1.Location.X - tarakan1.Width + 1)
+            {
+                Thread.Sleep(random.Next(10, 50));
+                this.Invoke((MethodInvoker)delegate
+                {
+                    tarakan1.Location = new Point(tarakan1.Location.X + random.Next(1, 5), tarakan1.Location.Y);
+                });
+            }
+            Tarakan1Finish();
+        }
+
+        private void Tarakan2Move()
+        {
+            while (tarakan2.Location.X <= pictureBox1.Location.X - tarakan2.Width + 1)
+            {
+                Thread.Sleep(random.Next(10, 50));
+                this.Invoke((MethodInvoker)delegate
+                {
+                    tarakan2.Location = new Point(tarakan2.Location.X + random.Next(1, 5), tarakan2.Location.Y);
+                });
+            }
+            Tarakan2Finish();
+        }
+
+        private void TarakanFinish()
+        {
+
+            this.Invoke((MethodInvoker)delegate
+            {
+                tarakan.Location = tarakanpoint;
+            });
+            threadTarakan1.Abort();
+            threadTarakan2.Abort();
+            this.Invoke((MethodInvoker)delegate
+            {
+                tarakan1.Location = tarakan1point;
+                tarakan2.Location = tarakan2point;
+            });
+            MessageBox.Show($"{tarakan.Name} win");
+        }
+        private void Tarakan1Finish()
+        {
+
+            this.Invoke((MethodInvoker)delegate
+            {
+                tarakan1.Location = tarakan1point;
+            });
+            threadTarakan.Abort();
+            threadTarakan2.Abort();
+            this.Invoke((MethodInvoker)delegate
+            {
+                tarakan.Location = tarakanpoint;
+                tarakan2.Location = tarakan2point;
+            });
+            MessageBox.Show($"{tarakan1.Name} win");
+        }
+        private void Tarakan2Finish()
+        {
+
+            this.Invoke((MethodInvoker)delegate
+            {
+                tarakan2.Location = tarakan2point;
+            });
+            threadTarakan.Abort();
+            threadTarakan1.Abort();
+            this.Invoke((MethodInvoker)delegate
+            {
+                tarakan.Location = tarakanpoint;
+                tarakan1.Location = tarakan1point;
+            });
+            MessageBox.Show($"{tarakan2.Name} win");
+        }
+    }
+}
